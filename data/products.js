bb81bfb64598a0355d51bed27fc32b06da12b219
Anuj <moreofme7777@gmail.com>
2025-02-24 02:05:33 +0530

    Load products through the fetch function (which returns the response as promise (only works with GET request)), instead of XHR class object (which uses callbacks).

--- data/products.js
+++ data/products.js
@@ -46,6 +46,27 @@ class Clothing extends Product {
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch('https://supersimplebackend.dev/products').then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('products fetched');
+  });
+
+  return promise;
+}
+
+// loadProductsFetch().then(() => {
+//   console.log('next step')
+// });
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
@@ -56,6 +77,7 @@ export function loadProducts(fun) {
       }
       return new Product(productDetails);
     });
+    console.log('products loaded');
 
     fun();
   })
